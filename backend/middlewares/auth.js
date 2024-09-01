ef9fe55900b0cb63212e6cffc503863eb088fe9d
Lb181808 <lb181808@gmail.com>
2024-09-01 16:22:22 +0530

    Created authentication middleware

---
+++ backend/middlewares/auth.js
@@ -0,0 +1,34 @@
+const jwt = require("jsonwebtoken");
+
+exports.auth = async (req, res, next) => {
+  try {
+    let token =
+      req.cookies.token ||
+      req.body.token ||
+      req.header("Authorization")?.replace("Bearer", "");
+
+    if (!token) {
+      return res.status(401).json({
+        success: false,
+        message: "Token is missing",
+      });
+    }
+
+    try {
+      const decoded = await jwt.verify(token, process.env.JWT_SECRET);
+      req.user = decoded;
+      next();
+    } catch (err) {
+      return res.status(401).json({
+        success: false,
+        message: "Token is invalid",
+      });
+    }
+  } catch (err) {
+    console.error("Error during token verification:", err);
+    return res.status(500).json({
+      success: false,
+      message: "Something went wrong while verifying the token",
+    });
+  }
+};
