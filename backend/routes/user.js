8a0a48b3c56c25369e53b4c48ac36d6a01a9b9e5
Lb181808 <lb181808@gmail.com>
2024-09-02 23:42:17 +0530

    modified user routes

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -4,10 +4,18 @@ const router=express.Router()
 
 const {
     sendOTP,
-    signUpCustomer
+    signUpCustomer,
+    signUpDriver,
+    loginUser,
+    logoutUser,
+    resetPassword
 }=require('../controllers/auth')
 
 router.post('/send-otp',sendOTP)
 router.post('/signupCustomer',signUpCustomer)
+router.post('/signupDriver', signUpDriver);
+router.post('/login', loginUser);
+router.post('/logout', logoutUser);
+router.post('/reset-password', auth, resetPassword);
 
 module.exports=router
