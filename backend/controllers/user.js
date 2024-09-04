41237e36ba0a89b9bdc64249aa7236b360361925
Lb181808 <lb181808@gmail.com>
2024-09-04 22:56:11 +0530

    Updated user profile ;

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -29,6 +29,52 @@ exports.getUserProfile=async(req,res)=>{
 
 exports.updateUserProfile=async(req,res)=>{
     try{
-        
+        const userId=req.user.userId;
+        const {name,phoneNumber,email}=req.body;
+
+        if (!name || !phoneNumber || !email) {
+            return res.status(400).json({
+              success: false,
+              message: "Name, phone number, and email are required.",
+            });
+          }
+
+          if (!emailRegex.test(email)) {
+            return res.status(400).json({
+              success: false,
+              message: "Invalid email format.",
+            });
+          }
+
+          if (!phoneRegex.test(phoneNumber)) {
+            return res.status(400).json({
+              success: false,
+              message: "Invalid phone number format.",
+            });
+          }
+
+          const updatedUser=await User.findByIdAndUpdate(userId,{
+            name,phoneNumber,email
+          },{new:true,runValidators:true})
+
+          if (!updatedUser) {
+            return res.status(404).json({
+              success: false,
+              message: "User not found.",
+            });
+          }
+
+          res.status(200).json({
+            success: true,
+            message: "Profile updated successfully.",
+            data: updatedUser,
+          });
     }
+    catch (err) {
+        console.error("Error updating user profile:", err);
+        res.status(500).json({
+          success: false,
+          message: "An error occurred while updating the profile. Please try again later.",
+        });
+      }
 }
